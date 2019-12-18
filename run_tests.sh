db389a6e77ddeba5447b1d35758c214f00916eb2
Vladimir Antonov <holixus@gmail.com>
2019-12-18 16:37:34 +0300

    feature: run_tests.sh script for run all options variations tests

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+#JSON_FLOATS
+#JSON_64BITS_INTEGERS
+#JSON_HEX_NUMBERS
+
+test_basic()
+{
+	echo "run test $*"
+	cmake $* . && make && ./tests
+}
+
+test_hex()
+{
+	test_basic $* -DJSON_HEX_NUMBERS=ON && test_basic $* -DJSON_HEX_NUMBERS=OFF
+}
+
+test_64bit()
+{
+	test_hex $* -DJSON_64BITS_INTEGERS=ON && test_hex $* -DJSON_64BITS_INTEGERS=OFF
+}
+
+test_floats()
+{
+	test_64bit -DJSON_FLOATS=ON && test_64bit -DJSON_FLOATS=OFF
+}
+
+test_floats
